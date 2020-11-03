d656693baa86f0af4a41179185f8346807bb41a7
muhammadreza <mra.akhgari@gmail.com>
2020-11-03 19:29:54 +0330

    Add the output folder

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from lexer import Lexer
 
 lexer = Lexer().build()
-with open('test-cases\\lexer\\test1.txt') as f:
+with open('test-cases\\lexer\\test3.txt') as f:
+    result = open('output\\lexer\\output3.txt', 'w')
     lexer.input(f.read())
     while True:
         tok = lexer.token()
         if not tok:
             break
-        print(tok)
+        result.write(str(tok) + '\n')
