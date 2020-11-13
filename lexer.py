9dca2aa76ca75def38e21dcfe63ee43c2d1bfb86
muhammadreza <mra.akhgari@gmail.com>
2020-11-13 19:31:37 +0330

    Add lexems to lexer

--- lexer.py
+++ lexer.py
@@ -6,7 +6,7 @@
 # ------------------------------------------------------------
 
 from ply import lex
-
+from lexems import *
 
 class Lexer:
 
@@ -30,35 +30,35 @@ class Lexer:
     ###
     global reserved
     reserved = {
-        'int': 'INTEGER',
-        'float': 'FLOAT',
-        'bool': 'BOOLEAN',
-        'fun': 'FUNCTION',
-        'True': 'TRUE',
-        'False': 'FALSE',
-        'print': 'PRINT',
-        'return': 'RETURN',
-        'main': 'MAIN',
-        'if': 'IF',
-        'else': 'ELSE',
-        'elseif': 'ELSEIF',
-        'while': 'WHILE',
-        'on': 'ON',
-        'where': 'WHERE',
-        'for': 'FOR',
-        'and': 'AND',
-        'or': 'OR',
-        'not': 'NOT',
-        'in': 'IN'
+        'int': INTEGER,
+        'float': FLOAT,
+        'bool': BOOLEAN,
+        'fun': FUNCTION,
+        'True': TRUE,
+        'False': FALSE,
+        'print': PRINT,
+        'return': RETURN,
+        'main': MAIN,
+        'if': IF,
+        'else': ELSE,
+        'elseif': ELSEIF,
+        'while': WHILE,
+        'on': ON,
+        'where': WHERE,
+        'for': FOR,
+        'and': AND,
+        'or': OR,
+        'not': NOT,
+        'in': IN
     }
 
     # List of token names.   This is always required
     tokens = [
-        'ID', 'INTEGERNUMBER', 'FLOATNUMBER',
-        'ASSIGN', 'SUM', 'SUB', 'MUL', 'DIV', 'MOD',
-        'GT', 'GE', 'LT', 'LE', 'EQ', 'NE',
-        'LCB', 'RCB', 'LRB', 'RRB', 'LSB', 'RSB',
-        'SEMICOLON', 'COLON', 'COMMA', 'ERROR'
+        ID, INTEGERNUMBER, FLOATNUMBER,
+        ASSIGN, SUM, SUB, MUL, DIV, MOD,
+        GT, GE, LT, LE, EQ, NE,
+        LCB, RCB, LRB, RRB, LSB, RSB,
+        SEMICOLON, COLON, COMMA, ERROR
     ] + list(reserved.values())
 
     # Regular expression rules for simple tokens
